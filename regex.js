896315a0707339d81ed2a83edd9828e0c1c5d53c
jane muthoni <muchirijane@outlook.com>
2020-07-03 12:02:34 +0300

    added regex match and test methods

--- regex.js
+++ regex.js
@@ -10,6 +10,7 @@ const result = str.replace(regex, replacement);
 
 console.log(result);
 */
+/*
 const regex = /^\[(.*?)\](\(.*?\))$/;
 const str = '[Google](http://www.google.com)';
 
@@ -19,3 +20,48 @@ const replacement = '<a href ="$2">$1</a>';
 const result = str.replace(regex, replacement);
 
 console.log(result);
+*/
+/* 
+//janetracy@SpeechGrammarList.com
+    const regex = /(\w+)@(\w+)\.([a-z]{2,8})([a-z]{2,8})?/;
+    const str = textarea.value;
+    console.log(str);
+
+
+    //const replacement = ;
+
+    const result = regex.test(str);
+    console.log(result);
+*/
+const button = document.querySelector('button');
+const textarea = document.querySelector('textarea');
+const resultText = document.querySelector('.result-text');
+
+function regexPattern (){
+    //janetracy@SpeechGrammarList.com
+    const regex = /\(?(\d{3}\)?)[-.](\d{3})[\-\.](\d{4})/g;
+    const str = textarea.value;
+    console.log(str);
+
+
+    const results = str.match(regex);
+     console.log(results);
+     
+    results.forEach(result =>{
+        let html = '';
+        html += `
+          
+            <li>${result}</li>
+        `;   
+
+        resultText.innerHTML += html;
+    })
+
+
+}
+
+button.addEventListener('click', () => {
+    resultText.innerHTML += `<P>This is what I matched</P>`
+
+    regexPattern();
+})
